31adc8a3e6a59956fb4517184e39925c395d2d7d
Jason Macnak <natsu@google.com>
2025-06-11 14:19:20 -0700

    Minor codegen changes
     * Pragma macros are typcially not indented
    
     * Use GFXSTREAM_ERROR instead of fprintf
    
     * Update the run command in the header comments

--- scripts/generate-apigen-sources.sh
+++ scripts/generate-apigen-sources.sh
@@ -12,6 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+set -e
 set -x
 
 bazel build codegen/generic-apigen:gfxstream_generic_apigen
