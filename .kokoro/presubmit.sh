f3832c0e8f0e98290aeb5f09046dcc0ee569fb57
Jason Macnak <natsu@google.com>
2025-07-10 10:29:20 -0700

    DO NOT SUBMIT: Testing images

--- .kokoro/presubmit.sh
+++ .kokoro/presubmit.sh
@@ -1,7 +1,19 @@
 #!/bin/bash
 
 # Fail on errors
-set -e
+#set -e
 set -x
 
 echo "Gfxstream kokoro tests will go here eventually..."
+
+pwd
+
+which cvd
+
+ls /usr/lib/cuttlefish-common
+
+ls /home/kbuilder
+
+ls /home/kbuilder/android-cts
+
+ls /home/kbuilder/android-cts/16
