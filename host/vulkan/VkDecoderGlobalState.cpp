70909718df74db39cdaf9a94303487b4243cb09b
Serdar Kocdemir <kocdemir@google.com>
2025-09-02 14:31:29 +0100

    Adjustments to some log messages
    Reduce usages of stderr printing and use gfxstream logging
    functions instead.
    
    Bug: 389637419
    Bug: 433496880
    Test: CI
    Change-Id: I52ad2fe9953cffa1b028b9a388e7f90c7141a9c0

--- host/vulkan/VkDecoderGlobalState.cpp
+++ host/vulkan/VkDecoderGlobalState.cpp
@@ -1423,10 +1423,10 @@ class VkDecoderGlobalState::Impl {
         } else {
             // No instance extension, fake it!!!!
             if (pFeatures->pNext) {
-                fprintf(stderr,
+                GFXSTREAM_WARNING(
                         "%s: Warning: Trying to use extension struct in "
                         "VkPhysicalDeviceFeatures2 without having enabled "
-                        "the extension!\n",
+                        "the extension!",
                         __func__);
             }
             *pFeatures = {
@@ -1578,10 +1578,10 @@ class VkDecoderGlobalState::Impl {
         } else {
             // No instance extension, fake it!!!!
             if (pImageFormatProperties->pNext) {
-                fprintf(stderr,
+                GFXSTREAM_WARNING(
                         "%s: Warning: Trying to use extension struct in "
                         "VkPhysicalDeviceFeatures2 without having enabled "
-                        "the extension!!!!11111\n",
+                        "the extension!",
                         __func__);
             }
             *pImageFormatProperties = {
@@ -1687,10 +1687,10 @@ class VkDecoderGlobalState::Impl {
             case WhichFunc::kGetPhysicalDeviceFormatProperties: {
                 // No instance extension, fake it!!!!
                 if (pFormatProperties->pNext) {
-                    fprintf(stderr,
+                    GFXSTREAM_WARNING(
                             "%s: Warning: Trying to use extension struct in "
                             "vkGetPhysicalDeviceFormatProperties2 without having "
-                            "enabled the extension!!!!11111\n",
+                            "enabled the extension!",
                             __func__);
                 }
                 pFormatProperties->sType = VK_STRUCTURE_TYPE_FORMAT_PROPERTIES_2;
@@ -1742,10 +1742,10 @@ class VkDecoderGlobalState::Impl {
         } else {
             // No instance extension, fake it!!!!
             if (pProperties->pNext) {
-                fprintf(stderr,
+                GFXSTREAM_WARNING(
                         "%s: Warning: Trying to use extension struct in "
                         "VkPhysicalDeviceProperties2 without having enabled "
-                        "the extension!!!!11111\n",
+                        "the extension!",
                         __func__);
             }
             *pProperties = {
@@ -1869,10 +1869,10 @@ class VkDecoderGlobalState::Impl {
         } else {
             // No instance extension, fake it!!!!
             if (pMemoryProperties->pNext) {
-                fprintf(stderr,
+                GFXSTREAM_WARNING(
                         "%s: Warning: Trying to use extension struct in "
                         "VkPhysicalDeviceMemoryProperties2 without having enabled "
-                        "the extension!!!!11111\n",
+                        "the extension!",
                         __func__);
             }
             *pMemoryProperties = {
@@ -2139,7 +2139,7 @@ class VkDecoderGlobalState::Impl {
             vk->vkGetPhysicalDeviceFeatures2(physicalDevice, &features2);
 
             if (mVerbosePrints) {
-                fprintf(stderr,
+                GFXSTREAM_VERBOSE(
                         "VERBOSE:%s: MoltenVK supportedPortabilityFeatures\n"
                         "constantAlphaColorBlendFactors = %d\n"
                         "events = %d\n"
@@ -3136,7 +3136,7 @@ class VkDecoderGlobalState::Impl {
 #ifdef CONFIG_AEMU
         if (bindInfoCount > 1 && snapshotsEnabled()) {
             if (mVerbosePrints) {
-                fprintf(stderr,
+                GFXSTREAM_WARNING(
                     "vkBindImageMemory2 with more than 1 bindInfoCount not supporting snapshot");
             }
             get_gfxstream_vm_operations().set_skip_snapshot_save(true);
@@ -5873,9 +5873,9 @@ class VkDecoderGlobalState::Impl {
                             importCbInfoPtr->colorBuffer);
 
                     if (cbExtMemoryHandle == nullptr) {
-                        fprintf(stderr,
+                        GFXSTREAM_ERROR(
                                 "%s: VK_ERROR_OUT_OF_DEVICE_MEMORY: "
-                                "colorBuffer 0x%x does not have Vulkan external memory backing\n",
+                                "colorBuffer 0x%x does not have Vulkan external memory backing",
                                 __func__, importCbInfoPtr->colorBuffer);
                         return VK_ERROR_OUT_OF_DEVICE_MEMORY;
                     }
@@ -5954,10 +5954,10 @@ class VkDecoderGlobalState::Impl {
                     m_vkEmulation->getBufferMetalMemoryHandle(importBufferInfoPtr->buffer);
 
                 if (bufferMetalMemoryHandle == nullptr) {
-                    fprintf(stderr,
+                    GFXSTREAM_ERROR(
                             "%s: VK_ERROR_OUT_OF_DEVICE_MEMORY: "
                             "buffer 0x%x does not have Vulkan external memory "
-                            "backing\n",
+                            "backing",
                             __func__, importBufferInfoPtr->buffer);
                     return VK_ERROR_OUT_OF_DEVICE_MEMORY;
                 }
@@ -6683,9 +6683,9 @@ class VkDecoderGlobalState::Impl {
         auto vk = dispatch_VkDevice(boxed_device);
 
         if (!m_vkEmulation->getFeatures().GlDirectMem.enabled) {
-            fprintf(stderr,
-                    "FATAL: Tried to use direct mapping "
-                    "while GlDirectMem is not enabled!\n");
+            GFXSTREAM_ERROR(
+                "FATAL: Tried to use direct mapping "
+                "while GlDirectMem is not enabled!");
         }
 
         std::lock_guard<std::mutex> lock(mMutex);
